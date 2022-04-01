b94573e7dc19147126a5fd43603a6b574c57928c
sargon64 <sargon64@tutanota.com>
2022-03-31 19:18:59 -0700

    fix: :zap: router construction macro

--- src/net/tcp/listener.rs
+++ src/net/tcp/listener.rs
@@ -130,27 +130,39 @@ impl Route {
     }
 }
 
+macro_rules! router{
+    { $($match_pattern:expr => $route:expr),+ } => {
+        {
+            let mut router = Router::new();
+            $(
+                router.add_route(Route::new(Regex::new($match_pattern).unwrap(), Arc::new($route)));
+            )+
+            router
+        }
+    };
+}
 
-// #[test]
-// fn test_router() {
-//     fn d_route(mut stream: TcpStream, _data: [u8; 128]) -> StatusCode {
-//         println!("called d_route");
-//         write!(stream, "d_route").unwrap();
+#[test]
+fn test_router() {
+    fn d_route(mut stream: TcpStream, _data: [u8; 128]) -> StatusCode {
+        println!("called d_route");
+        write!(stream, "d_route").unwrap();
 
-//         stream.flush().unwrap();
+        stream.flush().unwrap();
 
-//         //let mut buf = String::new();
+        //let mut buf = String::new();
 
-//         stream.read(&mut [0; 128]).unwrap();
+        stream.read(&mut [0; 128]).unwrap();
 
-//         //println!("{}", buf);
+        //println!("{}", buf);
 
-//         StatusCode::Ok.construct(stream)
-//     }
-//     let route = Route::new(Regex::new(".*").unwrap(), Arc::new(d_route));
-//     let mut router = Router::new();
-//     router.add_route(route);
-//     let mut server = Server::new("127.0.0.1", 8080, router, 10);
-//     server.construct();
-//     server.run();
-//}
+        StatusCode::Ok.construct(stream)
+    }
+    let router = router! {
+        ".*" => d_route
+    };
+
+    let mut server = Server::new("127.0.0.1", 8080, router, 10);
+    server.construct();
+    server.run();
+}
